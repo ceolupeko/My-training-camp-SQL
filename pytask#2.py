7f765730fcc346bd9ef12e7fa20c3aafc7850c0f
ceolupeko <darnistelsanker@gmail.com>
2022-01-26 14:01:35 +0200

    Commit of the second Python task from LeetCode

---
+++ pytask#2.py
@@ -0,0 +1,9 @@
+# Two Sum
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+# I didn't understand what's the difference in "substring" and "subsequence" so i did such
+
+# User input
+s = input("Your string: ")
+
+# Output unique subsequence
+print(len(set(s)))
