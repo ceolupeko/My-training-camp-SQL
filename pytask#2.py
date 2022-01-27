3d6856de748180c77430972bb2f02815d3022e92
ceolupeko <98124502+ceolupeko@users.noreply.github.com>
2022-01-27 11:02:21 +0200

    FEl

--- pytask#2.py
+++
@@ -1,9 +0,0 @@
-# Longest Substring Without Repeating Characters
-# https://leetcode.com/problems/longest-substring-without-repeating-characters/
-# I didn't understand what's the difference in "substring" and "subsequence" so i did such
-
-# User input
-s = input("Your string: ")
-
-# Output unique subsequence
-print(len(set(s)))
