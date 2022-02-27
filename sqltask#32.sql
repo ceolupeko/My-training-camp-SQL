829dc164592d7ea8a65a693d2357d006efd998ee
ceolupeko <darnistelsanker@gmail.com>
2022-02-27 13:11:47 +0200

    Commit of the thirty second SQL-task

---
+++ sqltask#32.sql
@@ -0,0 +1,8 @@
+# SQL Basics: Up and Down
+# https://www.codewars.com/kata
+
+select 
+  case when sum(number1) % 2 != 0 then min(number1) else max(number1) end as number1, 
+  case when sum(number2) % 2 != 0 then min(number2) else max(number2) end as number2
+
+from numbers
