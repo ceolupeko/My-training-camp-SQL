cdf783449081bb792b5382c4d2f4618cc6193f07
ceolupeko <darnistelsanker@gmail.com>
2022-03-03 19:52:33 +0200

    Commit of the fourty fourth SQL-task

---
+++ sqltask#44.sql
@@ -0,0 +1,7 @@
+# Register for the Party (SQL for Beginners #3)
+# https://www.codewars.com/kata
+
+insert into participants(name, age, attending)
+values ('John', 23, true);
+
+select * from participants
